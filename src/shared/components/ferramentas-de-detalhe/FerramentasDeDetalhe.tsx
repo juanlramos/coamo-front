2e42981da47782dfc2dde44e887861118178860b
Juan Ramos <juanl.bnr@gmail.com>
2024-09-30 17:21:47 -0300

    feat: implementado os services de acessoa o backend

--- src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -107,7 +107,7 @@ export const FerramentasDeDetalhe: React.FC<IFerramentasDeDetalheProps> = ({
               textOverflow="ellipsis"
               overflow="hidden"
             >
-              Salvar e Voltar
+              Salvar e Fechar
             </Typography>
           </Button>
         )}
